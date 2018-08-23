775db34843faac7e267f48b857720395daee4492
Joey Cho <cjy12182000@naver.com>
2018-08-23 08:47:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,20 @@
 class Application
   @@item = []
   @@cart = []
-  
+
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     binding.pry
     if req.path.match(/items/)
+      item_title = req.path.split("/items/").last
+
+      item = @@items.find{|i| i.title == song_title}
+
+      if item !=nil
+        resp.write "The #{item.title} is #{item.price}"
+      end
       resp.write "Found testing"
     else
       resp.write "Route not found"
