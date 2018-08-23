afa12edb73a5ed7ff90acce357484739b7b2be8f
Joey Cho <cjy12182000@naver.com>
2018-08-23 08:48:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
     if req.path.match(/items/)
       item_title = req.path.split("/items/").last
 
@@ -15,7 +15,7 @@ class Application
       if item !=nil
         resp.write "The #{item.title} is #{item.price}"
       end
-      resp.write "Found testing"
+
     else
       resp.write "Route not found"
       resp.status = 404
