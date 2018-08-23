3d43092ab8dfd8bc6ae606e720c516eb71a624ed
Joey Cho <cjy12182000@naver.com>
2018-08-23 08:40:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    binding.pry
     if req.path.match(/testing/)
     else
       resp.write "Route not found"
